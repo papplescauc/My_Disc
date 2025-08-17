09774d6fd62dd2912aceba8bd9b87dacef5ee567
Silvio Roussos <96267404+papplescauc@users.noreply.github.com>
2025-08-17 14:47:08 -0400

    error handle & python_function

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ def add_course_view():
     
     return render_template('add_course.html')
 
-
-
 @app.route('/current_round')
 def current_round_view():
     return render_template('current_round.html')
@@ -25,6 +23,19 @@ def current_round_view():
 def login_view():
     return render_template('login.html')
 
+@app.route('/python_function/')
+def python_function():
+    print('yooooo we did a thing')
+    1/0
+    return {'data': 'this is the data'}
+
+@app.errorhandler(Exception)
+def error_handler(err: Exception):
+    response = {
+        'error': err.__class__.__name__,
+        'message': str(err)
+    }
+    return response, 500
 
 
 if __name__ == "__main__":
--- static/js/add_course.js
+++ static/js/add_course.js
@@ -1,20 +1,52 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('hello world');
 
-    document.getElementById('submit-button').addEventListener('click', function(mouseEvent) {
+    document.getElementById('submit-button').addEventListener('click', async function(mouseEvent) {
         /** @type {HTMLFormElement} */ const form = document.getElementById('main-form');
         form.reportValidity();
         mouseEvent.preventDefault();
 
+
         // display loading spinner
 
         // add course (call python code, wait for it to complete)
-
+        let data = null;
+        try {
+            const response = await safeFetch('/python_function', 'Error calling python function');
+            data = response['data'];
+        }
+        catch (err) {
+            alert(err);
+            throw err;
+        }
+        
+        console.log(data);
+        
         // hide loading spinner
 
     });
 
 
 });
+
+
+
+async function safeFetch(url, err_msg = 'Fetch Error', options = {}) {
+
+    const response = await fetch(url, options);
+
+    /** @type {Promise<Object>} */ let data = null;
+    try {
+        data = await response.json();
+    }
+    catch (jsonParseError) {
+        throw new Error(`${err_msg}: Error parsing JSON during safeFetch: ${jsonParseError}`);
+    }
+    if (!response.ok) {
+        throw new Error(`${err_msg}: ${data.error || 'Unexpected error'}: ${data.message || 'no further error info provided'}`);
+    }
+
+    return data;
+}
 
 
